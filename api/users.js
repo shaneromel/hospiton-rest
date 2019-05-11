69bfd323eb2311996ab551e022931574c6e6e30d
shaneromel <shaneromelkujur@gmail.com>
2019-05-11 09:42:52 +0530

    changes

--- api/users.js
+++ api/users.js
@@ -19,6 +19,43 @@ require("../utils/mongodb").then(db=>{
 
     })
 
+    router.post("/toggle-favorite", (req, res)=>{
+        const data=req.body;
+        const userUid=data.user_uid;
+        const doctorUid=data.doctor_uid;
+
+        userCollection.findOne({uid:userUid, favorite_doctors:doctorUid}, (err, result)=>{
+            if(err){
+                res.send({code:"error", message:err.message});
+                return;
+            }
+
+            if(result){
+                userCollection.updateOne({uid:userUid}, {$pull:{favorite_doctors:{$in:[doctorUid]}}}, (err, result)=>{
+                    if(err){
+                        res.send({code:"error", message:err.message});
+                        return;
+                    }
+
+                    res.send({code:"success"});
+
+                })
+            }else{
+                userCollection.updateOne({uid:userUid}, {$push:{favorite_doctors:doctorUid}}, (err, result)=>{
+                    if(err){
+                        res.send({code:"error", message:err.message});
+                        return;
+                    }
+
+                    res.send({code:"success"});
+
+                })
+            }
+
+        })
+
+    })
+
 });
 
 module.exports=router;
